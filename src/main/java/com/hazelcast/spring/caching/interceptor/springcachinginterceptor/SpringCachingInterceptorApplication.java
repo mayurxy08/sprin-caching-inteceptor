6b06d7cdb195131e6756bb416a09e86c24991e96
unknown <mayurxy08@gmail.com>
2023-05-07 15:41:07 -0400

    base code added

---
+++ src/main/java/com/hazelcast/spring/caching/interceptor/springcachinginterceptor/SpringCachingInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.hazelcast.spring.caching.interceptor.springcachinginterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCachingInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCachingInterceptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazelcast/spring/caching/interceptor/springcachinginterceptor/SpringCachingInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelcast.spring.caching.interceptor.springcachinginterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCachingInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
