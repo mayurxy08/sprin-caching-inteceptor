76e588efdb5f5f971a9445da5cea86a1740abeee
mayur <4680580+mayurxy08@users.noreply.github.com>
2023-05-08 05:23:33 +0000

    added entrylistener for cached messages

--- src/main/java/com/hazelcast/spring/caching/interceptor/springcachinginterceptor/SpringCachingInterceptorApplication.java
+++ src/main/java/com/hazelcast/spring/caching/interceptor/springcachinginterceptor/SpringCachingInterceptorApplication.java
@@ -4,17 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.Hazelcast;
+import com.hazelcast.map.MapInterceptor;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.map.listener.EntryAddedListener;
+import com.hazelcast.map.listener.EntryRemovedListener;
+import com.hazelcast.map.listener.EntryUpdatedListener;
+import com.hazelcast.core.*;
+import com.hazelcast.map.IMap;
 
 @SpringBootApplication
 public class SpringCachingInterceptorApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Config config = new Config();
+        config.setProperty("hazelcast.map.entry.filtering.natural.event.types", "true");
+        HazelcastInstance cache = Hazelcast.newHazelcastInstance(config);
+        IMap<String, String> map = cache.getMap("cachedMessages");
+        map.addEntryListener(new MyEntryListener(), true);
+        System.out.println("Entry Listener registered");
+    }
 
-		SpringApplication.run(SpringCachingInterceptorApplication.class, args);
-		
-		HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
-        var cache =  hazelcastInstance.getMap("cachedMessages");
-        cache.addInterceptor(new MessageInterceptor());
-	}
+    static class MyEntryListener implements EntryAddedListener<String, String> {
 
+        @Override
+        public void entryAdded(EntryEvent<String, String> event) {
+            System.out.println("****new message Added :" + event.getValue());
+        }
+    }
 }
